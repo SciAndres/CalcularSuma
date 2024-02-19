c30bc47bd5678b4f5183e1e7203cbda32dbe5fc8
Andres_enriquez <andres_alb.enriquez@uao.edu.co>
2024-02-19 17:00:38 -0500

    Se sube codigo con modificaciones

--- CalSum.py
+++ CalSum.py
@@ -1,9 +1,9 @@
 # Pedir al usuario que ingrese dos números
 num1 = float(input("Ingresa el primer número: "))
 num2 = float(input("Ingresa el segundo número: "))
-
+num3 =  float(input("Ingresa el tercer número: "))
 # Sumar los números
-suma = num1 + num2
+suma = num1 + num2 + num3
 
 # Mostrar el resultado
-print("La suma de", num1, "y", num2, "es igual a", suma)
+print("La suma de", num1, num2, "y", num3, "es igual a", suma)
